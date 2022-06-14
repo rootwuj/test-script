e95ce409f12e268098e29fe95b5b2f8523fa7a5e
Jing Wu <Jing.Wu@suse.com>
2022-06-14 13:37:37 +0800

    update

--- script/ha-autok3s/0-ca.sh
+++ script/ha-autok3s/0-ca.sh
@@ -2,15 +2,13 @@
 
 # ========== 使用方法 ===========
 
-# ./0-ca.sh 172.1.2.xx 172.1.2.xx 172.1.2.xx
+# ./0-ca.sh 172.1.2.xx,172.1.2.xx,172.1.2.xx
 
 # ==============================
 
 
 # 传入3个节点的内网IP
-HOST_1=$1
-HOST_2=$2
-HOST_3=$3
+HOST=$1
 
 
 # 生成域名证书
@@ -19,7 +17,7 @@ cd /root/ca
 wget https://raw.githubusercontent.com/rootwuj/test-script/main/script/ha-rke1/tls.sh
 chmod +x tls.sh
 
-./tls.sh self.wujing.site $HOST_1 $HOST_2 $HOST_3
+./tls.sh self.wujing.site $HOST
 
 # 生成localhost证书
 mkdir /root/ca-local
@@ -27,6 +25,6 @@ cd /root/ca-local
 wget https://raw.githubusercontent.com/rootwuj/test-script/main/script/ha-rke1/tls.sh
 chmod +x tls.sh
 
-./tls.sh localhost $HOST_1 $HOST_2 $HOST_3
+./tls.sh localhost $HOST
 
 
