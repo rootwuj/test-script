b4c4fb280e120d85fe25826e61b6992ae70ef37f
Jing Wu <42061003+rootwuj@users.noreply.github.com>
2022-07-07 14:23:33 +0800

    Update 3-helm-ca.sh

--- script/ha-rke1/3-helm-ca.sh
+++ script/ha-rke1/3-helm-ca.sh
@@ -2,14 +2,15 @@
 
 # ========== 使用方法 ===========
 
-# ./3-helm-ca.sh 172.1.2.xx,172.1.2.xx,172.1.2.xx
+# ./3-helm-ca.sh rke4 172.1.2.xx,172.1.2.xx,172.1.2.xx
 
 # ==============================
 
 
+# 传入rke版本，部署对应的helm
+RKE_VERSION=$1
 # 传入3个节点的内网IP
-HOST=$1
-
+HOST=$2
 
 # 生成域名证书
 mkdir /root/ca
@@ -31,17 +32,30 @@ chmod +x tls.sh
 # 安装helm
 cd /root
 
-# 2.4 需要使用3.4.x及以下版本helm
-# wget https://get.helm.sh/helm-v3.4.1-linux-amd64.tar.gz
-# tar -zxvf helm-v3.4.1-linux-amd64.tar.gz
 
-# 适用于2.5环境
-# wget https://get.helm.sh/helm-v3.6.3-linux-amd64.tar.gz
-# tar -zxvf helm-v3.6.3-linux-amd64.tar.gz
+case $RKE_VERSION in
+    1)  
+    # 2.4 需要使用3.4.x及以下版本helm
+    wget https://get.helm.sh/helm-v3.4.1-linux-amd64.tar.gz
+    tar -zxvf helm-v3.4.1-linux-amd64.tar.gz
 
-# 适用于2.6环境
-wget https://get.helm.sh/helm-v3.8.2-linux-amd64.tar.gz
-tar -zxvf helm-v3.8.2-linux-amd64.tar.gz
+    ;;
+    2)  
+    # 适用于2.5环境
+    wget https://get.helm.sh/helm-v3.6.3-linux-amd64.tar.gz
+    tar -zxvf helm-v3.6.3-linux-amd64.tar.gz
+
+    ;;
+    3)  
+    # 适用于2.6环境
+    wget https://get.helm.sh/helm-v3.8.2-linux-amd64.tar.gz
+    tar -zxvf helm-v3.8.2-linux-amd64.tar.gz
+
+    ;;
+    *)  echo 'ERROR: 你没有输入正确的版本，例如rke4(2.4的rke)'
+    ;;
+esac
+
 
 mv linux-amd64/helm /usr/local/bin/helm
 helm version
